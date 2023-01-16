323edef43ab6d29e62fddaee44db254c1832acf2
Gabor Szabo <gabor@szabgab.com>
2023-01-16 10:33:55 +0200

    add site generation

---
+++ generate.py
@@ -0,0 +1,51 @@
+import os
+import pathlib
+import shutil
+from jinja2 import Environment, FileSystemLoader
+
+def render(template, filename, **args):
+    templates_dir = pathlib.Path(__file__).parent.joinpath('templates')
+    env = Environment(loader=FileSystemLoader(templates_dir), autoescape=True)
+    html_template = env.get_template(template)
+    html_content = html_template.render(**args)
+    with open(filename, 'w') as fh:
+        fh.write(html_content)
+
+def main():
+    os.makedirs('_site', exist_ok=True)
+
+    for filename in os.listdir('sound'):
+        shutil.copy(os.path.join('sound', filename), os.path.join('_site', filename))
+
+    pages = []
+    md_files = os.listdir('text')
+    for md_file in md_files:
+        print(md_file)
+        with open(os.path.join('text', md_file)) as fh:
+            fh.readline()
+            page = {}
+            page['id'] = md_file[0:-3]
+            for row in fh:
+                if row.startswith('---'):
+                    break
+                # print(row)
+                field, value = row.split(':')
+                page[field] = value
+
+            page['text'] = fh.read()
+            #print(page)
+            pages.append(page)
+
+    render('index.html', '_site/index.html',
+        title = "Haberes Buenos de Buly",
+        pages = pages,
+    )
+    for page in pages:
+        render('page.html', f'_site/{page["id"]}.html',
+            title = page['titulo'],
+            page = page,
+        )
+
+
+
+main()
